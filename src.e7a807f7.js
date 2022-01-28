e8a84fbc060ec492aee88686fe0b84d64b359346
GodDet1 <93922168+GodDet1@users.noreply.github.com>
2022-01-28 08:43:47 +0000

    Deploying to gh-pages from @ GodDet1/Ice-cream@1129d0e5c2887e548b9705abf96605e1b44f9bea 🚀

--- src.e7a807f7.js
+++ src.e7a807f7.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/Ice-cream/src.1372f34d.js.map
+//# sourceMappingURL=/Ice-cream/src.e7a807f7.js.map
