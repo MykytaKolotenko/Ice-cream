5e95295664cf14a59a062ee01cfaa2ad90440efc
Nataliia <pokoruknataliia@gmail.com>
2022-01-29 17:04:30 +0200

    update

--- src/read-more.js
+++ src/read-more.js
@@ -1,19 +1,23 @@
-let btns = document.querySelectorAll(".btn");
+//Поиск всех кнопок с классом btn
+let btns = document.querySelectorAll('.products-card__btn');
+//Цикл по всем кнопкам
 for (btn of btns) {
-    btn.addEventListener("click", function () {
-        let card = this.closest(".products-card");
-        let dots = card.querySelector(".dots");
-        let more = card.querySelector(".more");
-
-        if (dots.style.display === "none") {
-            dots.style.display = "inline";
-            more.style.display = "none";
-        }
-        else {
-            dots.style.display = "none";
-            more.style.display = "inline";
-            
-        }
-    });
-    
+  //Обработка события клика для каждой кнопки
+  btn.addEventListener('click', function() {
+    //Поиск родителя у текущей кнопки с классом card
+    let card = this.closest('.products-card')
+    //Поиск Точек у родителя (класс dots)
+    let dots = card.querySelector('.dots');
+    //Поиск Текста у родителя (класс more)
+    let more = card.querySelector('.more');
+    //Если точек нет то скрыть текст и показхать точки
+    if(dots.style.display === 'none') {
+      dots.style.display = 'inline';
+      more.style.display = 'none';
+    } else {
+      //Иначе показать текст и скрыть точки
+      dots.style.display = 'none';
+      more.style.display = 'inline';
+    }
+  });
 }
