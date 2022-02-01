8a1c084b3952aca7fa8d10271029317794dc293a
Yana Mironyak <yanika.mironyak@gmail.com>
2022-02-01 15:45:48 +0200

    Update

--- src/js/modal-map.js
+++ src/js/modal-map.js
@@ -2,7 +2,7 @@
   const refs = {
     openModalBtn: document.querySelector('[data-modal-opn]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    modal: document.querySelector('[data-modal-maps]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
